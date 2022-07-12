e0b4b6f82f713af06efbb51f7859883bb9db40b9
vaishnaviauti <vaishnaviauti2003@gmail.com>
2022-07-12 11:50:15 +0530

    add file

---
+++ week2/day1/main.js
@@ -0,0 +1,26 @@
+
+
+let email;
+let password;
+let student_name;
+let college_name;
+let branch;
+let Phone_number;
+let address;
+
+function account(){
+    alert("warning")
+    email=document.getElementById("email").value;
+    password=document.getElementById("password").value;
+    student_name=document.getElementById("student_name").value;
+    college_name=document.getElementById("college_name").value;
+    branch=document.getElementById("branch").value;
+    Phone_number=document.getElementById("Phone_number").value;
+    address=document.getElementById("address").value;
+    document.write("student name: "+student_name );
+    document.write("<br>student email: "+email );
+    document.write("<br>college name: "+college_name );
+    document.write("<br>branch name: "+branch );
+    document.write("<br>phone number: "+Phone_number );
+    document.write("<br>address:"+address);
+}
